e37ce40dd8343c7f4a3c426030bcc1a802aa20b2
lcb <1582340306@qq.com>
2024-06-11 16:47:13 +0800

    1.Upgrade kotlin version to 2.0.0 2.迁移到 versioncatlogs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,13 +2,15 @@ import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
-    id("com.google.devtools.ksp")
-    id("org.jetbrains.kotlin.plugin.serialization")
-    id("androidx.room")
+    alias(libs.plugins.agp)
+    alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.kotlin.serialization)
+    alias(libs.plugins.room)
 }
 
+composeCompiler {}
 
 android {
     namespace = "com.lcb.one"
@@ -48,11 +50,6 @@ android {
     buildFeatures {
         compose = true
         buildConfig = true
-    }
-    composeOptions {
-        // Compose 与 Kotlin 的兼容性对应关系：
-        // https://developer.android.com/jetpack/androidx/releases/compose-kotlin
-        kotlinCompilerExtensionVersion = "1.5.6"
     }
     packaging {
         resources {
@@ -102,62 +99,45 @@ project.afterEvaluate {
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.activity:activity-compose:1.8.2")
+    implementation(libs.androidx.core)
     // compose
-    // bom对照表：https://developer.android.google.cn/develop/ui/compose/bom/bom-mapping
-    val composeVersion = "2024.04.00"
-    implementation(platform("androidx.compose:compose-bom:$composeVersion"))
-    implementation("androidx.compose.ui:ui")
-    implementation("androidx.compose.ui:ui-graphics")
-    implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3")
-    implementation("androidx.compose.material:material-icons-extended")
-    implementation("androidx.compose.runtime:runtime-livedata")
-    implementation("androidx.constraintlayout:constraintlayout-compose:1.0.1")
-    implementation("androidx.navigation:navigation-compose:2.7.7")
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.bundles.androidx.compose)
+    implementation(libs.bundles.androidx.compose.support)
 
-    val lifecycleVersion = "2.7.0"
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycleVersion")
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:$lifecycleVersion")
-    implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
-
-    implementation("androidx.preference:preference-ktx:1.2.1")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
-
+    implementation(libs.kolor)
+    implementation(libs.coil.compose)
+    implementation(libs.zoomimage.compose.coil)
+    // 协程
+    implementation(libs.coroutines.android)
+    // lifecycle
+    implementation(libs.bundles.androidx.lifecycle)
+    // preference
+    implementation(libs.androidx.preference)
     // glance
-    implementation("androidx.glance:glance-appwidget:1.0.0")
-
-    // 图片加载 coil
-    implementation("io.coil-kt:coil-compose:2.4.0")
-
-    // 网络请求 retrofit2
-    implementation("com.squareup.retrofit2:retrofit:2.11.0")
-    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
-    implementation("com.squareup.retrofit2:converter-scalars:2.6.2")
-    implementation("com.squareup.retrofit2:converter-kotlinx-serialization:2.11.0")
-
+    implementation(libs.androidx.glance)
+    // retrofit2
+    implementation(libs.bundles.retrofit2)
     // json
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
-
+    implementation(libs.kotlinx.serialization.json)
     // markdown
-    implementation("io.noties.markwon:core:4.6.2")
+    implementation(libs.markdown)
+    // room
+    implementation(libs.androidx.room)
+    implementation(libs.androidx.room.runtime)
+    annotationProcessor(libs.androidx.room.compiler)
+    ksp(libs.androidx.room.compiler)
 
-    // room数据库
-    val roomVersion = "2.6.1"
-    implementation("androidx.room:room-runtime:$roomVersion")
-    implementation("androidx.room:room-ktx:$roomVersion")
-    annotationProcessor("androidx.room:room-compiler:$roomVersion")
-    ksp("androidx.room:room-compiler:$roomVersion")
+    // for test
+    addTestDependencies()
+}
 
-    implementation("io.github.panpf.zoomimage:zoomimage-compose-coil:1.0.2")
-    implementation("com.materialkolor:material-kolor:1.4.4")
-
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:$composeVersion"))
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
-    debugImplementation("androidx.compose.ui:ui-tooling")
-    debugImplementation("androidx.compose.ui:ui-test-manifest")
+fun DependencyHandlerScope.addTestDependencies() {
+    testImplementation(libs.test.junit)
+    androidTestImplementation(libs.test.ext.junit)
+    androidTestImplementation(libs.test.espresso)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.test.ui.junit4)
+    debugImplementation(libs.test.ui.tooling)
+    debugImplementation(libs.test.ui.manifest)
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,10 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    val kotlinVersion = "1.9.21"
-    val kspVersion = "1.9.21-1.0.15"
-    val androidApplicationVersion = "8.1.2"
-
-    id("com.android.application") version androidApplicationVersion apply false
-    id("org.jetbrains.kotlin.android") version kotlinVersion apply false
-    id("org.jetbrains.kotlin.jvm") version kotlinVersion apply false
-    id("org.jetbrains.kotlin.plugin.serialization") version "1.6.10" apply false
-    id("com.google.devtools.ksp") version kspVersion apply false
-
-    id("androidx.room") version "2.6.1" apply false
+    alias(libs.plugins.agp) apply false
+    alias(libs.plugins.kotlin) apply false
+    alias(libs.plugins.kotlin.compose) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.ksp) apply false
+    alias(libs.plugins.kotlin.serialization) apply false
+    alias(libs.plugins.room) apply false
 }
